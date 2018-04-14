eaa285d3c6feeee3f048b9a11228b9b179f54512
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-14 15:00:14 +0300

    ~ SearchView as Observable

--- app/src/main/java/pro/busik/test/weather/SearchFragment.kt
+++ app/src/main/java/pro/busik/test/weather/SearchFragment.kt
@@ -4,13 +4,20 @@ import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.view.*
 import android.support.v7.widget.SearchView
+import com.jakewharton.rxbinding2.support.v7.widget.RxSearchView
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableObserver
 import pro.busik.test.weather.utils.SafeLog
+import pro.busik.test.weather.utils.plusAssign
+import java.util.concurrent.TimeUnit
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 class SearchFragment : Fragment() {
+
+    private val compositeDisposable = CompositeDisposable()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -25,17 +32,26 @@ class SearchFragment : Fragment() {
         val searchMenuItem = menu!!.findItem(R.id.action_search)
 
         val searchView = searchMenuItem.actionView as SearchView
-        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String): Boolean {
-                SafeLog.v("onQueryTextSubmit: ${query}")
-                searchMenuItem.collapseActionView()
-                return true
-            }
+        compositeDisposable += RxSearchView.queryTextChanges(searchView)
+                .debounce(500, TimeUnit.MILLISECONDS)
+                .subscribeWith(object : DisposableObserver<CharSequence>() {
+                    override fun onComplete() {
+                        SafeLog.v("onComplete()")
+                    }
 
-            override fun onQueryTextChange(query: String): Boolean {
-                SafeLog.v("onQueryTextChange: ${query}")
-                return true
-            }
-        })
+                    override fun onNext(value: CharSequence?) {
+                        SafeLog.v("onNext(): ${value}")
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        SafeLog.v("onError() ${e}")
+                    }
+                })
+
+    }
+
+    override fun onDestroyOptionsMenu() {
+        super.onDestroyOptionsMenu()
+        compositeDisposable.dispose()
     }
 }
