7c11d57af071ab2621db4923d6543c80b5a04a96
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-14 14:59:26 +0300

    + RxExtensions

---
+++ app/src/main/java/pro/busik/test/weather/utils/RxExtensions.kt
@@ -0,0 +1,8 @@
+package pro.busik.test.weather.utils
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+operator fun CompositeDisposable.plusAssign(disposable: Disposable) {
+    add(disposable)
+}
