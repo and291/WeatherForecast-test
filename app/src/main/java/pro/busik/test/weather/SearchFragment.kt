6fc70496b9c3dc76c57c2a28c25f5888d2cf87d4
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-14 13:29:43 +0300

    ~ SearchFragment

--- app/src/main/java/pro/busik/test/weather/SearchFragment.kt
+++ app/src/main/java/pro/busik/test/weather/SearchFragment.kt
@@ -9,10 +9,10 @@ import android.view.ViewGroup
 /**
  * A placeholder fragment containing a simple view.
  */
-class MainActivityFragment : Fragment() {
+class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_search, container, false)
     }
 }
