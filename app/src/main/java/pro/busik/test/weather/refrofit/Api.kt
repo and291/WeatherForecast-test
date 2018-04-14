21e2f70c27922206a7cd8281fa50d0e7fbf45e9c
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-14 17:43:10 +0300

    + Api forecast()

--- app/src/main/java/pro/busik/test/weather/refrofit/Api.kt
+++ app/src/main/java/pro/busik/test/weather/refrofit/Api.kt
@@ -2,6 +2,7 @@ package pro.busik.test.weather.refrofit
 
 import io.reactivex.Observable
 import pro.busik.test.weather.model.FindResponse
+import pro.busik.test.weather.model.ForecastResponse
 import retrofit2.http.GET
 import retrofit2.http.Headers
 import retrofit2.http.Query
@@ -10,5 +11,9 @@ interface Api {
     @Headers("Content-Type: text/json; charset=utf-8")
     @GET("data/2.5/find?type=like&lang=ru&mode=json&appid=0a49791459a25a1ec88fb1893630d971")
     fun find(@Query("q") query: String) : Observable<FindResponse>
+
+    @Headers("Content-Type: text/json; charset=utf-8")
+    @GET("/data/2.5/forecast?lang=ru&mode=json&appid=0a49791459a25a1ec88fb1893630d971")
+    fun forecast(@Query("q") query: String) : Observable<ForecastResponse>
 }
 
