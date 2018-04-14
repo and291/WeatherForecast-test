a42869f33caab40010c9086669cbcf6a3e82dcf3
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-14 22:34:51 +0300

    ~ ForecastResponse.getEmptyResponse()

--- app/src/main/java/pro/busik/test/weather/model/ForecastResponse.kt
+++ app/src/main/java/pro/busik/test/weather/model/ForecastResponse.kt
@@ -1,4 +1,8 @@
 package pro.busik.test.weather.model
 
 data class ForecastResponse(val city: City,
-                            val list: ArrayList<ForecastItem>)
+                            val list: ArrayList<ForecastItem>) {
+    companion object {
+        fun getEmptyResponse() = ForecastResponse(City(0, "none"), arrayListOf())
+    }
+}
