d2c7823f12d5f0a5513233f19866456e085cd6f5
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-14 14:20:23 +0300

    + toolbar SearchView for SearchFragment

--- app/src/main/java/pro/busik/test/weather/SearchFragment.kt
+++ app/src/main/java/pro/busik/test/weather/SearchFragment.kt
@@ -2,9 +2,10 @@ package pro.busik.test.weather
 
 import android.support.v4.app.Fragment
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.support.v7.widget.SearchView
+import pro.busik.test.weather.utils.SafeLog
+
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,6 +14,28 @@ class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater?.inflate(R.menu.fragment_search, menu)
+        val searchMenuItem = menu!!.findItem(R.id.action_search)
+
+        val searchView = searchMenuItem.actionView as SearchView
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                SafeLog.v("onQueryTextSubmit: ${query}")
+                searchMenuItem.collapseActionView()
+                return true
+            }
+
+            override fun onQueryTextChange(query: String): Boolean {
+                SafeLog.v("onQueryTextChange: ${query}")
+                return true
+            }
+        })
     }
 }
