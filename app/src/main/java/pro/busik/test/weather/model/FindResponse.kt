b7840cfa79592291db7c02b2e93f6c09c0ab1e96
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-16 13:46:21 +0300

    - redundant code

--- app/src/main/java/pro/busik/test/weather/model/FindResponse.kt
+++
@@ -1,6 +0,0 @@
-package pro.busik.test.weather.model
-
-data class FindResponse(val cod: Int,
-                        val count: Int,
-                        val list: ArrayList<City>?,
-                        val message: String)
