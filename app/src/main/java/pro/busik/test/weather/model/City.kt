96e1b794ae1237f5a7e93ee98c5302a01858b369
DESKTOP-MKM871P\and2919 <and2919@gmail.com>
2018-04-17 20:31:45 +0300

    ~ City : parcelable

--- app/src/main/java/pro/busik/test/weather/model/City.kt
+++ app/src/main/java/pro/busik/test/weather/model/City.kt
@@ -1,4 +1,31 @@
 package pro.busik.test.weather.model
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class City(val id: Int,
-                val name: String?)
+                val name: String?) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readInt(),
+            parcel.readString()) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeInt(id)
+        parcel.writeString(name)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<City> {
+        override fun createFromParcel(parcel: Parcel): City {
+            return City(parcel)
+        }
+
+        override fun newArray(size: Int): Array<City?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
